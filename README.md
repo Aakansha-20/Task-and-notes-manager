e644e7c99e4c6f1f5df53c28c60936165c9f48b5
Aakansha-20 <aakansha.bhardwaj2000@gmail.com>
2020-04-17 04:31:46 +0530

    'master'

--- README.md
+++
@@ -1,1 +0,0 @@
-# Task-and-notes-manager
--- public/index.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    let list=$('#list')
-    fetchTodos(function(task){
-        list.empty()
-        for(t of task){
-            list.append(createTask(t))
-        }
-    })
-})
